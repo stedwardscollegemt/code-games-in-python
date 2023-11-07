5ea83c193be8bc1dc51fbe54c401e55b8ca42bbe
erikacamilleri <erika.camilleri@gmail.com>
2023-11-07 19:57:15 +0100

    Create turtle_maze.py

---
+++ ch01_getting_started/turtle_maze.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+# enter maze
+forward(25)
+
+# up
+left(90)
+forward(50)
+
+# right
+right(90)
+forward(25)
+
+# todo: other instructions to guide the turtle to the apple
+
+quit = input("Press any key to quit...")

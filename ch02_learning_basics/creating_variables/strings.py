b3f2a5bdb8b0b4aeba4c8de547a2413152e74a1d
erikacamilleri <erika.camilleri@gmail.com>
2023-11-24 10:44:50 +0100

    Teacher model answer for strings.py

--- ch02_learning_basics/creating_variables/strings.py
+++ ch02_learning_basics/creating_variables/strings.py
@@ -20,15 +20,31 @@ title()
 # Player: P
 maze_row_1 = "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"
 maze_row_2 = "█ .      ▀▀  . . P █"
-# your code... get creative :)
+maze_row_3 = "█ . █              █"
+maze_row_4 = "█ .            █ . █"
+maze_row_5 = "█ . ▀▀▀▀▀▀▀▀▀▀▀▀ . █"
+maze_row_6 = "█ .      ▀▀  . . . █"
+maze_row_7 = "█ . . . . . . . .  █"
+maze_row_8 = "█ . ▀▀▀▀▀▀▀▀▀▀▀▀ * █"
+maze_row_9 = "█ . █              █"
+maze_row_10 = "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
 
-# todo: write print statements under each other to display to entire maze
-# your code
+# write print statements under each other to display to entire maze
+print(maze_row_1)
+print(maze_row_2)
+print(maze_row_3)
+print(maze_row_4)
+print(maze_row_5)
+print(maze_row_6)
+print(maze_row_7)
+print(maze_row_8)
+print(maze_row_9)
+print(maze_row_10)
 
 # display the width of our maze
 maze_row_length = len(maze_row_1)
 print("Maze Width: ", maze_row_length)
 
-# todo: display the number of food items '.' in the entire maze
-food_count = maze_row_1.count(".") + maze_row_2.count(".")
-# your code...
+# display the number of food items '.' in the entire maze
+food_count = maze_row_1.count(".") + maze_row_2.count(".") + maze_row_3.count(".") + maze_row_4.count(".") + maze_row_5.count(".") + maze_row_6.count(".") + maze_row_7.count(".") + maze_row_8.count(".") + maze_row_9.count(".") + maze_row_10.count(".")
+print("Food Count: ", food_count)

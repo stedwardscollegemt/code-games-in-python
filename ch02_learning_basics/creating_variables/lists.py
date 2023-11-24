089d30ba6a5edaa5dd8feebf415bf16e3c4166f8
erikacamilleri <erika.camilleri@gmail.com>
2023-11-24 11:05:23 +0100

    Practice with lists.py

---
+++ ch02_learning_basics/creating_variables/lists.py
@@ -0,0 +1,38 @@
+# ------ import modules section -----------------------------------------
+
+# ------ define functions section ---------------------------------------
+def word_puzzle():
+    print("J   L   I   B   P   N   Z   Q   O   A   J   D")
+    print("K   B   F   A   M   Z   S   B   E   A   R   O")
+    print("O   A   K   T   M   I   C   E   C   T   Q   G")
+    print("Y   L   L   S   H   O   E   D   A   O   G   U")
+    print("S   L   H   C   O   W   Z   B   T   Y   A   H")
+    print("M   H   A   N   D   S   A   O   I   S   L   A")
+    print("T   O   P   I   F   Y   P   Y   A   G   J   T")
+    print("E   Z   T   B   E   L   T   E   A   T   A   H")
+
+# ------ algorithm steps section ----------------------------------------
+
+# todo: declare a list variable called all_words that contains words the player can find in the puzzle e.g., "BALL", and "BOYE"
+# your code...
+
+print("*** Weclome to Word Play ***")
+print("****************************")
+print("")
+
+word_puzzle()
+
+# todo: display an instruction to the user e.g. "Can you find all the words?"
+# your code...
+
+# todo: ask the user to enter a word e.g., "Enter a word: "
+# your code...
+
+# todo: use the list function count() to see if the user input is a good guess, store it in a variable called hit
+# your code...
+
+# todo: display the value of the hit variable
+# your code...
+
+
+

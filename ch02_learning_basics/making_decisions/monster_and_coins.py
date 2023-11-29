206387ddc56c28c4244292d04c5c5818ab790343
erikacamilleri <erika.camilleri@gmail.com>
2023-11-29 11:21:56 +0100

    Teacher model answer for monster_and_coins

--- ch02_learning_basics/making_decisions/monster_and_coins.py
+++ ch02_learning_basics/making_decisions/monster_and_coins.py
@@ -10,11 +10,20 @@ print("Do we have more coins than monsters?")
 answer = coins > monsters
 print(answer)
 
-# todo: display question "Are monsters equal to coins?"
-# your code...
-# todo: update the answer variable with the correct boolean expression that compares coins and monster
-# your code...
-# todo: print answer value
-# your code ...
-# todo: less than
-# todo: more than one comparison
+# display question "Are monsters equal to coins?"
+print("Are monsters equal to coins?")
+# update the answer variable with the correct boolean expression that compares coins and monster
+answer = coins == monsters
+# print answer value
+print(answer)
+
+
+# less than
+print("Do we have less coins than monsters?")
+answer = coins < monsters
+print(answer)
+
+# more than one comparison
+print("Did you win the game?")
+answer = (coins > monsters) or (coins == monsters)
+print(answer)

72551f58a1a8afc36e3190eb455c4f83711eb209
erikacamilleri <erika.camilleri@gmail.com>
2024-01-10 11:29:54 +0100

    Teacher answer guess_the_number.py

--- ch02_learning_basics/playing_with_loops/guess_the_number.py
+++ ch02_learning_basics/playing_with_loops/guess_the_number.py
@@ -11,19 +11,28 @@ print("************************")
 # get a random number using the random module
 number = random.randint(1, 10)
 
-# todo: display a message "We have thought of a number, can you guess?"
-# your code...
+# display a message "We have thought of a number, can you guess?"
+print("We have thought of a number, can you guess?")
 
 # accept a guess input from the user
-guess_in = input("Enter your guess between 1 and 10")
+guess_in = input("Enter your guess between 1 and 10:  ")
 
 # convert user guess into an integer so that we can make comparisons
 guess = int(guess_in)
 
-# todo: create a boolean variable is_guessed that checks whether guess is equal to number
-# your code...
+# create a boolean variable is_guessed that checks whether guess is equal to number
+is_guessed = (guess == number)
 
-# todo: ask for the user to keep guessing while not is_guessed... else, the user won!
-# your code...
+# ask for the user to keep guessing while not is_guessed... otherwise, the user won!
+chances = 3
+while(not is_guessed):
+    guess_in = input("Enter another guess between 1 and 10:  ")
+    guess = int(guess_in)
+    is_guessed = (guess == number)
+    chances = chances - 1
+    if chances == 0:
+        print("Sorry you did not guess in four chances! The number was", number)
+        break
 
-# todo: why not include a break and give the user three chances to guess?
+if is_guessed:
+    print("Wow! You won.")

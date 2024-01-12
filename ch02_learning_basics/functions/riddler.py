b09180a21a5cbeebc8aab031d4ca1c9200d8c69f
erikacamilleri <erika.camilleri@gmail.com>
2024-01-12 10:11:52 +0100

    Learn about functions and solve a riddle.

---
+++ ch02_learning_basics/functions/riddler.py
@@ -0,0 +1,23 @@
+# ------ import modules section -----------------------------------------
+# todo: import time
+
+# ------ define functions section ---------------------------------------
+def title():
+    print("+-+-+-+-+-+-+-+ +-+-+-+-+ +-+-+-+-+")
+    print("        |R|I|D|D|L|E|R|            ")
+    print("+-+-+-+-+-+-+-+ +-+-+-+-+ +-+-+-+-+")
+    
+# ------ algorithm steps section ----------------------------------------
+title();
+
+riddle = "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?"
+clue = "You would probably hear it in a large cave."
+answer = "echo"
+
+# todo: Let us code a typing effect to display the riddle using functions
+#       - print(char, end="", flush=True)
+#       - time.sleep(delay)
+
+# todo: Give the user two chances to guess, for the second chance display the clue
+
+# todo: Display whether the user won or not, in which case the answer will be revealed

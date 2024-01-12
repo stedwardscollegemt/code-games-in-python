a5048a3f1d89c46a3dbaf40b87cbc85fb124b81e
erikacamilleri <erika.camilleri@gmail.com>
2024-01-12 11:41:08 +0100

    WIP Started typing effect.

--- ch02_learning_basics/functions/riddler.py
+++ ch02_learning_basics/functions/riddler.py
@@ -1,5 +1,5 @@
 # ------ import modules section -----------------------------------------
-# todo: import time
+import time
 
 # ------ define functions section ---------------------------------------
 def title():
@@ -8,7 +8,7 @@ def title():
     print("+-+-+-+-+-+-+-+ +-+-+-+-+ +-+-+-+-+")
     
 # ------ algorithm steps section ----------------------------------------
-title();
+title()
 
 riddle = "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?"
 clue = "You would probably hear it in a large cave."
@@ -17,6 +17,9 @@ answer = "echo"
 # todo: Let us code a typing effect to display the riddle using functions
 #       - print(char, end="", flush=True)
 #       - time.sleep(delay)
+for char in riddle:
+    print(char, end="", flush=True)
+    time.sleep(0.3)
 
 # todo: Give the user two chances to guess, for the second chance display the clue
 

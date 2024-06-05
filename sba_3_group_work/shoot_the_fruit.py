177710984e618f5c4cd49a82d5e442800d953238
erikacamilleri <erika.camilleri@gmail.com>
2024-06-05 10:23:58 +0200

    Update shoot_the_fruit.py

--- sba_3_group_work/shoot_the_fruit.py
+++ sba_3_group_work/shoot_the_fruit.py
@@ -15,22 +15,36 @@ fruits = [grapes, banana]
 random_fruit = random.choice(fruits)
 screen_text = "Hit the fruit!"
 
-# todo: let us add numerical variables for max_rounds, current_round and points
+# let us add numerical variables for max_rounds, current_round and points
+max_rounds = 10
+current_round = 0
+points = 0
 
-# todo: let us add a boolean variable for game_over
+# let us add a boolean variable for game_over
+game_over = False
 
 # ---- define functions --------------------
 def draw():
     screen.clear()
-    # todo: let us improve this code so that it "knows" when the game is over
-    random_fruit.draw()
-    screen.draw.text(screen_text, topleft=(10,10))
+    # let us improve this code so that it "knows" when the game is over
+    if game_over:
+        screen.draw.text("Game Over!", center=(WIDTH // 2, HEIGHT // 2), fontsize=50, color="red")
+        screen.draw.text(screen_text, center=(WIDTH // 2, HEIGHT // 2 + 50), fontsize=30, color="white")
+    else:
+        random_fruit.draw()
+        screen.draw.text(screen_text, topleft=(10,10))
 
 def on_mouse_down(pos):
-    # todo: we need to make a lot of improvements here for our game to be playable e.g., check whether game is over, track rounds
-    global screen_text, random_fruit
+    # we need to make a lot of improvements here for our game to be playable e.g., check whether game is over, track rounds
+    global game_over, screen_text, random_fruit, points, current_round, max_rounds
+    if game_over:
+        return
     if random_fruit.collidepoint(pos):
         screen_text = "Wow, good aim!"
+        points = points + 5
+        current_round = current_round + 1
+        if current_round >= max_rounds:
+            game_over = True
         random_fruit = random.choice(fruits)
         place_fruit(random_fruit)
     else:
